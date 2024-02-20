c8f8964a6c2aeadee706df97a42b74bb09736681
Michael Smith <michael.smith.ok@gmail.com>
2024-02-20 17:25:06 -0600

    Add preloading for bg images

--- spa/src/app/app.routes.ts
+++ spa/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { StatsOverviewComponent } from './stats-overview/stats-overview.component';
 import { StatsUpcomingEventsComponent } from './stats-upcoming-events/stats-upcoming-events.component';
 
+
+// WHEN ADDING BG, ALSO ADD TO INDEX.HTML FOR PRELOADING
 export const routes: Routes = [
   { path: '', component: StatsOverviewComponent, data: { bg: "assets/img/swing_feet.jpg", next: '/upcoming' } },
   { path: 'upcoming', component: StatsUpcomingEventsComponent, data: { bg: "assets/img/swingout.jpg", previous: '/' } },
