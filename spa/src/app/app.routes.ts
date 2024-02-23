5b2c3c93cb7489864e53fe3c25eb1607270d1721
Michael Smith <michael.smith.ok@gmail.com>
2024-02-23 16:02:46 -0600

    Add about us page

--- spa/src/app/app.routes.ts
+++ spa/src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { DancerPageComponent } from './dancer-page/dancer-page.component';
 import { EventPageComponent } from './event-page/event-page.component';
 import { DancersPageComponent } from './dancers-page/dancers-page.component';
 import { EventsPageComponent } from './events-page/events-page.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
 
 
 // WHEN ADDING BG, ALSO ADD TO INDEX.HTML FOR PRELOADING
 export const routes: Routes = [
   { path: '', component: StatsOverviewComponent, data: { bg: "assets/img/swing_feet.jpg", next: '/upcoming' } },
   { path: 'upcoming', component: StatsUpcomingEventsComponent, data: { bg: "assets/img/swingout.jpg", previous: '/', next: '/on-the-rise' } },
-  { path: 'on-the-rise', component: StatsOnTheRiseComponent, data: { bg: "assets/img/courthouse.jpg", previous: '/upcoming', next: '/dancers' } },
-  { path: 'dancers', component: DancersPageComponent, data: { bg: "assets/img/swingdanceuk.jpg", previous: '/on-the-rise', next: '/events' } },
+  { path: 'on-the-rise', component: StatsOnTheRiseComponent, data: { bg: "assets/img/courthouse.jpg", previous: '/upcoming', next: '/about/us' } },
+  { path: 'about/us', component: AboutUsPageComponent, data: { bg: "assets/img/mechstack.jpg", previous: '/on-the-rise', next: '/dancers' } },
+  { path: 'dancers', component: DancersPageComponent, data: { bg: "assets/img/swingdanceuk.jpg", previous: '/about/us', next: '/events' } },
   { path: 'events', component: EventsPageComponent, data: { bg: "assets/img/jitterbug.jpg", previous: '/dancers' } },
   { path: 'dancers/:id', component: DancerPageComponent, data: { bg: "assets/img/pulledaway.jpg", previous: '_' } },
   { path: 'events/:id', component: EventPageComponent, data: { bg: "assets/img/locarnoballroom.jpg", previous: '_' } },
