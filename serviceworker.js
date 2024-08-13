c8f302b434deb91bff4cb030eb37dc16d74c8714
smwa <michael.smith.ok@gmail.com>
2024-08-13 15:19:10 +0000

    Deploying to gh-pages from  - 745d38ee38c6c26456704060f05de9df3266cb43 🚀

--- serviceworker.js
+++ serviceworker.js
@@ -1,15 +1,31 @@
 const cacheName = 'static_site_cache_v1';
 
+const reloadedAssets = {};
+
 self.addEventListener("install", (event) => {});
 
 self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
   event.respondWith(caches.open(cacheName).then((cache) => {
     return cache.match(event.request).then((cachedResponse) => {
-      const fetchedResponse = fetch(event.request).then((networkResponse) => {
-        cache.put(event.request, networkResponse.clone());
+      let fetchedResponse = null;
+      if (!cachedResponse || !(url in reloadedAssets)) {
+        fetchedResponse = fetch(event.request).then(async (networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
 
-        return networkResponse;
-      });
+          reloadedAssets[url] = true;
+          const client = await self.clients.get(event.clientId);
+          if (client) {
+            client.postMessage({
+              type: "fetched",
+              url: url,
+            });
+          }
+          
+          return networkResponse;
+        });
+
+      }
 
       return cachedResponse || fetchedResponse;
     });
